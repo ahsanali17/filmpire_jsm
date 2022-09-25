606b5e0e7f4df83bf3f106dcffa3298c5c3b676b
ahsanali17 <ahsantime1@gmail.com>
2022-09-25 17:13:29 -0400

    added themeprovider to the index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider} from '@mui/material/styles';
 import App from './components/App.jsx';
 
+const theme = createTheme({});
 const root = document.getElementById('root');
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
   root,
 );
 
