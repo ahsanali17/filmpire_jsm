741930abd30f396fe82434a96dea1626cee58668
ahsanali17 <ahsantime1@gmail.com>
2023-02-17 03:29:58 -0500

    small fixes made, done app, i will consider adding the ai later on

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -99,7 +99,7 @@ function NavBar() {
                 <Avatar
                   style={{ width: 30, height: 30 }}
                   alt="Profile"
-                  src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"
+                  src={`https://www.themoviedb.org/t/p/w64_and_h64_face${user.avatar.tmdb.avatar_path}`}
                 />
               </Button>
             )}
